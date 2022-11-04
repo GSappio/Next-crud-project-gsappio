f7c0034e608fbcdd03aeb3d26b3a286a8621cb9c
Gabriel Sappio <100438648+GSappio@users.noreply.github.com>
2022-11-04 11:20:31 -0300

    Uploading necessary files

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  purge: [
+  
+  "./src/pages/**/*.{js,ts,jsx,tsx}",
+  
+  "./src/components/**/*.{js,ts,jsx,tsx}",
+  
+  ],
+  
+  darkMode: false,
+
+  theme: {
+  
+  extend: {},
+  
+  },
+  
+  plugins: [],
+  
+  }
